7040f762a7f7214a807099988931df2243a117bb
Rahul <102817224+rahulsalgo@users.noreply.github.com>
2023-03-16 22:33:58 +0530

    this is the repo regarding the strings and the method which we use in strings

---
+++ script.js
@@ -0,0 +1,34 @@
+let names="rahul"
+console.log(names.length)
+
+let a="mango"
+let b= "banana"
+let sentence= `${a} and ${b} are fruits`
+console.log(sentence)
+
+let fruit="banana"
+console.log(fruit)
+let fruit1="ban\nnan"
+console.log(fruit1)
+let fruit2="bananan\"a"
+console.log(fruit2)
+let fruit3="mang\r\ro"
+console.log(fruit3)
+
+let string1="rahulmishra"
+console.log(string1.length)
+console.log(string1.slice(2,11))
+// console.log(string1.slice(2,10))
+// console.log(string1.slice(2,8))
+
+console.log(string1.toLowerCase())
+console.log(string1.toUpperCase())
+console.log(string1.replace("mishra","brahman"))
+// console.log(string1)
+
+for(let i=0;i<string1.length;i++)
+{
+    console.log(string1[i])
+}
+
+console.log(string1)
